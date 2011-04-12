ff618aebac71583f92957681f4af620c9b40a911
robertisele <robertisele@googlemail.com>
2011-04-12 12:21:56 +0200

    Defined restriction operators

--- ldif/ldif-core/src/main/scala/ldif/resource/Restriction.scala
+++ ldif/ldif-core/src/main/scala/ldif/resource/Restriction.scala
@@ -1,7 +1,27 @@
 package ldif.resource
 
-class Restriction
+case class Restriction(operator : Restriction.Operator)
 {
   //TODO
   def toSparql = ""
+}
+
+object Restriction
+{
+  sealed trait Operator
+
+  /**
+   * A condition which evaluates to true if the provided path contains at least one of the given values.
+   */
+  case class Condition(path : Path, values : Set[String]) extends Operator
+
+  /**
+   * Negates the provided operator.
+   */
+  case class Not(op : Operator)
+
+  /**
+   * Evaluates to true if all provided operators evaluate to true.
+   */
+  case class And(children : Traversable[Operator]) extends Operator
 }
