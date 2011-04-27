a8e9244f171e4836e16b82cdd282ed009fac3a75
a-matteini <a.matteini@mes-info.de>
2011-04-27 13:41:21 +0200

    Remote dump loader (draft) and minor changes

---
+++ ldif/ldif-local/src/main/scala/ldif/local/util/HttpClientFactory.scala
@@ -0,0 +1,36 @@
+package ldif.local.util
+
+//import org.apache.http.conn.params.ConnManagerParams
+//import org.apache.http.conn.scheme.{PlainSocketFactory,Scheme,SchemeRegistry}
+//import org.apache.http.impl.client.DefaultHttpClient
+//import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager
+//import org.apache.http.params.{BasicHttpParams, CoreProtocolPNames, HttpProtocolParams }
+//import org.apache.http.HttpVersion
+
+object HttpClientFactory  {
+  val MAX_CONNECTIONS = 100;
+  val HTTP_USER_AGENT = "ldif/0.1 (http://www.wiwiss.fu-berlin.de/en/institute/pwo/bizer/index.html)";
+  val HTTP_SOCKET_TIMEOUT = 30 * 1000;
+
+	def createHttpClient = {
+    //	    /* Create and initialize HTTP parameters */
+    //	    val params = new BasicHttpParams
+    //	    ConnManagerParams.setMaxTotalConnections(params, MAX_CONNECTIONS)
+    //	    HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1)
+    //		  params.setIntParameter("http.connection.timeout", HTTP_SOCKET_TIMEOUT)
+    //		  params.setParameter(CoreProtocolPNames.USER_AGENT, HTTP_USER_AGENT)
+    //
+    //	    /* Create and initialize scheme registry */
+    //	    val schemeRegistry = new SchemeRegistry
+    //	    schemeRegistry.register( new Scheme("http", PlainSocketFactory.getSocketFactory, 80))
+    //
+    //	    /*
+    //	     * Create an HttpClient with the ThreadSafeClientConnManager.
+    //	     * This connection manager must be used if more than one thread will
+    //	     * be using the HttpClient.
+    //	     */
+    //	    val cm = new ThreadSafeClientConnManager(params, schemeRegistry)
+    //
+    //      new DefaultHttpClient(cm, params)
+	}
+}
