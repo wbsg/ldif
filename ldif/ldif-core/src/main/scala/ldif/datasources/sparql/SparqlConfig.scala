8461e31024b769efd1eeca526823a0c713026700
a-matteini <a.matteini@mes-info.de>
2011-05-04 16:14:58 +0200

    Removed sparql module

--- ldif/ldif-core/src/main/scala/ldif/datasources/sparql/SparqlConfig.scala
+++
@@ -1,7 +0,0 @@
-package ldif.datasources.sparql
-
-import ldif.module.ModuleConfig
-
-case class SparqlConfig(endpoints : Traversable[EndpointConfig]) extends ModuleConfig
-{
-}
--- ldif/ldif-core/src/main/scala/ldif/datasources/sparql/SparqlTask.scala
+++
@@ -1,6 +0,0 @@
-package ldif.datasources.sparql
-
-import ldif.module.ModuleTask
-import ldif.util.Identifier
-
-class SparqlTask(override val name : Identifier, val endpoint : EndpointConfig) extends ModuleTask
