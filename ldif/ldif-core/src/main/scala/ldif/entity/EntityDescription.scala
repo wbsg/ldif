b6337dcc71f5bbadc27d6914a1eca79906c93955
robertisele <robertisele@googlemail.com>
2011-05-09 18:17:13 +0200

    Added XML de-/serialization to EntityDescription

--- ldif/ldif-core/src/main/scala/ldif/entity/EntityDescription.scala
+++ ldif/ldif-core/src/main/scala/ldif/entity/EntityDescription.scala
@@ -1,3 +1,49 @@
 package ldif.entity
 
-case class EntityDescription(restrictions : Restriction, patterns : IndexedSeq[IndexedSeq[Path]])
+import xml.Elem
+import ldif.util.Prefixes
+
+case class EntityDescription(restriction : Restriction, patterns : IndexedSeq[IndexedSeq[Path]])
+{
+  def toXML(implicit prefixes : Prefixes = Prefixes.empty) =
+  {
+    <EntityDescription>
+      { restriction.toXml }
+      <Patterns>
+      {
+        for(pattern <- patterns) yield
+        {
+          <Pattern>
+          {
+            for(path <- pattern) yield
+            {
+              <Path>{path.serialize}</Path>
+            }
+          }
+          </Pattern>
+        }
+      }
+      </Patterns>
+    </EntityDescription>
+  }
+}
+
+object EntityDescription
+{
+  def fromXML(xml : Elem)(implicit prefixes : Prefixes = Prefixes.empty) =
+  {
+    EntityDescription(
+      restriction = Restriction.fromXML(xml \ "Restriction" head),
+      patterns =
+          for(patternNode <- (xml \ "Patterns" \ "Pattern").toIndexedSeq[scala.xml.Node]) yield
+          {
+            for(pathNode <- (patternNode \ "Path").toIndexedSeq[scala.xml.Node]) yield
+            {
+              Path.parse(pathNode.text)
+            }
+          }
+    )
+  }
+}
+
+
