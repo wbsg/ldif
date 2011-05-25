4a4cf761ea450fd117a951d7edab0745c8e8e828
Robert Isele <robertisele@googlemail.com>
2011-05-25 18:10:29 +0200

    Integrated conversion parser

--- ldif/ldif-modules/ldif-silk/ldif-silk-common/src/main/scala/ldif/modules/silk/CreateEntityDescriptions.scala
+++ ldif/ldif-modules/ldif-silk/ldif-silk-common/src/main/scala/ldif/modules/silk/CreateEntityDescriptions.scala
@@ -4,17 +4,18 @@ import ldif.entity.Restriction.Condition
 import de.fuberlin.wiwiss.silk.instance.InstanceSpecification
 import ldif.entity.{Path, Restriction, EntityDescription}
 import de.fuberlin.wiwiss.silk.linkspec.LinkSpecification
+import de.fuberlin.wiwiss.silk.config.Prefixes
 
 /**
  * Generates EntityDescriptions from a LinkSpecification.
  */
-object CreateEntityDescriptions extends (LinkSpecification => Seq[EntityDescription])
+object CreateEntityDescriptions
 {
   /**
    * Generates EntityDescriptions from a LinkSpecification.
    */
-  def apply(linkSpec : LinkSpecification) : Seq[EntityDescription] =
+  def apply(linkSpec : LinkSpecification)(implicit prefixes : Prefixes) : Seq[EntityDescription] =
   {
-    InstanceSpecification.retrieve(linkSpec).toSeq.map(ConvertInstanceSpecification)
+    InstanceSpecification.retrieve(linkSpec).toSeq.map(ConvertInstanceSpecification.apply)
   }
 }
