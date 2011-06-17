d47e5ed43ac3e87ca53598af1a6665892cc63304
andreas <andreas@chin.(none)>
2011-06-17 20:29:11 +0200

    Finished FileQuadReader/Writer + Test

--- ldif/ldif-local/src/main/scala/ldif/local/runtime/impl/FileQuadReader.scala
+++ ldif/ldif-local/src/main/scala/ldif/local/runtime/impl/FileQuadReader.scala
@@ -1,8 +1,8 @@
 package ldif.local.runtime.impl
 
 import ldif.local.runtime.{Quad, QuadReader}
-import java.io.File
-import java.io.{ObjectInputStream, FileInputStream, BufferedInputStream}
+import java.io._
+import java.lang.RuntimeException
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,12 +12,48 @@ import java.io.{ObjectInputStream, FileInputStream, BufferedInputStream}
  * To change this template use File | Settings | File Templates.
  */
 
-class FileQuadReader extends QuadReader { //TODO: IMPLEMENT!
-  def hasNext = false
+class FileQuadReader(val inputFile: File) extends QuadReader {
+  val objectInput = new ObjectInputStream(new BufferedInputStream(new FileInputStream(inputFile)))
+  var closed = false
+  var bufferedQuad: Quad = null
+  var buffered = false
 
-  def read() = null
+  def hasNext: Boolean = {
+    if(closed)
+      return false
 
-  def isEmpty = false
+    if(buffered)
+      return true
 
-  def size = 0
+    val o = objectInput.readObject()
+    if(!o.isInstanceOf[Quad])
+      throw new RuntimeException("FileQuadReader read non-Quad object from stream. Object of class " + o.getClass + ": " + o)
+
+    val quad = o.asInstanceOf[Quad]
+    if(quad==NoQuadsLeft) {
+      closed = true
+      objectInput.close()
+      false
+    } else {
+      bufferedQuad = quad
+      buffered = true
+      true
+    }
+  }
+
+  def read(): Quad = {
+    if(buffered) {
+      buffered = false
+      return bufferedQuad
+    }
+
+    if(hasNext)
+      read()
+    else
+      throw new RuntimeException("No Quads left in FileQuadReader! Use hasNext-method before calling read-method.")
+  }
+
+  def isEmpty = !hasNext
+
+  def size = throw new RuntimeException("Method 'size' not implemented in FileQuadReader")
 }
