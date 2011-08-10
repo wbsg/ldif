8539dc81e470d1ab1d7c854085ef6815aba68f1c
a-matteini <a.matteini@mes-info.de>
2011-08-10 16:31:28 +0200

    Avoid row duplication in factumTables

---
+++ ldif/ldif-local/src/test/scala/RowTest.scala
@@ -0,0 +1,13 @@
+package
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: andrea
+ * Date: 8/10/11
+ * Time: 3:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class RowTest {
+
+}
---
+++ ldif/ldif-local/src/test/scala/ldif/local/RowTest.scala
@@ -0,0 +1,46 @@
+package ldif.local
+
+import org.junit.runner.RunWith
+import org.scalatest.FlatSpec
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.ShouldMatchers
+import util.JenaResultSetEntityBuilderHelper.Row
+import ldif.entity.Node
+
+@RunWith(classOf[JUnitRunner])
+class RowTest extends FlatSpec with ShouldMatchers {
+
+  it should "compare rows correctly" in {
+    val row1 = new Row
+    val row2 = new Row
+
+    val node1 = Node.createUriNode("http://aaa")
+    val node2 = Node.createUriNode("http://bbb")
+    val node3 = Node.createUriNode("http://ccc")
+
+    row1.append(node1)
+
+    row1.equals(row2) should equal (false)
+
+    row2.append(node1)
+
+    row1.equals(row2) should equal (true)
+    row1.hashCode.equals(row2.hashCode) should equal (true)
+
+    row1.append(node2)
+    row1.append(node3)
+
+    row2.append(node2)
+    row2.append(node3)
+
+    row1.equals(row2) should equal (true)
+    row1.hashCode.equals(row2.hashCode) should equal (true)
+
+    row1.append(node2)
+    row2.append(node3)
+
+    row1.equals(row2) should equal (false)
+
+  }
+
+}
