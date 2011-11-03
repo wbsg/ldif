064bbb6c2b462b3b85fbc724f0fa591609080c39
Andreas Schultz <a.schultz@fu-berlin.de>
2011-11-03 13:01:31 +0100

    minor changes

--- ldif/ldif-modules/ldif-entitybuilder/ldif-entitybuilder-mapreduce/src/main/scala/de/fuberlin/wiwiss/ldif/mapreduce/reducers/EntityConstructionReducer.scala
+++ ldif/ldif-modules/ldif-entitybuilder/ldif-entitybuilder-mapreduce/src/main/scala/de/fuberlin/wiwiss/ldif/mapreduce/reducers/EntityConstructionReducer.scala
@@ -7,6 +7,7 @@ import collection.mutable.{HashMap, HashSet, ArrayBuffer}
 import ldif.mapreduce.types._
 import ldif.entity.{EntityDescriptionMetadata, EntityWritable}
 import ldif.mapreduce.utils.HadoopHelper
+import de.fuberlin.wiwiss.ldif.mapreduce.ResultBuilder
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,18 +19,21 @@ import ldif.mapreduce.utils.HadoopHelper
 
 class EntityConstructionReducer extends MapReduceBase with Reducer[EntityDescriptionNodeWritable, ValuePathWritable, IntWritable, EntityWritable] {
   var edmd: EntityDescriptionMetadata = null
+  var resultBuilder: ResultBuilder = null
 
   override def configure(conf: JobConf) {
     edmd = HadoopHelper.getEntityDescriptionMetaData(conf)
+    resultBuilder = new ResultBuilder(edmd)
   }
 
   override def reduce(key: EntityDescriptionNodeWritable, values: Iterator[ValuePathWritable], output: OutputCollector[IntWritable, EntityWritable], reporter: Reporter) {
     val entityDescriptionID = key.entityDescriptionID.get
     val patternPaths = new HashMap[Int, ValuePathWritable]
     val restrictionPathValues = new HashSet[ValuePathWritable]
-    while(values.hasNext) {
-      val value = values.next()
-      output.collect(key.entityDescriptionID, new EntityWritable())
-    }
+
+    val valuePaths = new ArrayBuffer[ValuePathWritable]()
+    while(values.hasNext)
+      valuePaths.append(values.next())
+    resultBuilder.computeResultTables(entityDescriptionID, valuePaths)
   }
 }
