c832edb0bf0e33dd1deca1e72ddf3e375db66059
a-matteini <a.matteini@mes-info.de>
2011-12-14 15:16:42 +0100

    Changed silk output format to sequence file

--- ldif/ldif-modules/ldif-silk/ldif-silk-hadoop/src/main/scala/ldif/modules/silk/hadoop/SameAsOutputFormat.scala
+++ ldif/ldif-modules/ldif-silk/ldif-silk-hadoop/src/main/scala/ldif/modules/silk/hadoop/SameAsOutputFormat.scala
@@ -1,32 +1,57 @@
 package ldif.modules.silk.hadoop
 
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
-import org.apache.hadoop.io.Text
 import de.fuberlin.wiwiss.silk.hadoop.impl.EntityConfidence
-import org.apache.hadoop.mapreduce.{RecordWriter, TaskAttemptContext}
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat._
-import de.fuberlin.wiwiss.silk.hadoop.SilkConfiguration
-import java.io.DataOutputStream
+import org.apache.hadoop.io.{NullWritable, SequenceFile, Text}
+import ldif.hadoop.types.QuadWritable
+import ldif.datasources.dump.QuadParser
+import org.apache.hadoop.util.ReflectionUtils
+import org.apache.hadoop.io.compress.{DefaultCodec, CompressionCodec}
+import org.apache.hadoop.io.SequenceFile.CompressionType
+import org.apache.hadoop.mapreduce.{RecordWriter, TaskAttemptContext}
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat
 
-class SameAsOutputFormat extends FileOutputFormat[Text, EntityConfidence] {
+
+class SameAsOutputFormat extends SequenceFileOutputFormat[Text, EntityConfidence] {
 
   override def getRecordWriter(job : TaskAttemptContext) : RecordWriter[Text, EntityConfidence] = {
     val config = job.getConfiguration
-    val file = getDefaultWorkFile(job, ".nt")
+    val file = getDefaultWorkFile(job, "")
     val fs = file.getFileSystem(config)
-    val out = fs.create(file, false)
 
-    new LinkWriter(out)
+    val codec = getCompressionCodec(job)
+    val compressionType = getCompressionType(job)
+    val writer = SequenceFile.createWriter(fs, config, file, classOf[NullWritable], classOf[QuadWritable], compressionType, codec)
+    new LinkWriter(writer)
   }
 
-  private class LinkWriter(out : DataOutputStream) extends RecordWriter[Text, EntityConfidence] {
+  private class LinkWriter(writer : SequenceFile.Writer) extends RecordWriter[Text, EntityConfidence] {
     override def write(sourceUri : Text, entitySimilarity : EntityConfidence) {
       val line = "<" + sourceUri + "> <http://www.w3.org/2002/07/owl#sameAs> <" + entitySimilarity.targetUri + "> .\n"
-      out.write(line.getBytes("UTF-8"))
+      val quadParser = new QuadParser()
+      writer.append(NullWritable.get, new QuadWritable(quadParser.parseLine(line)))
     }
 
     override def close(context : TaskAttemptContext) {
-      out.close()
+      writer.close()
     }
+  }
+
+  // Get the CompressionType for the output SequenceFile
+  def getCompressionType(context : TaskAttemptContext) : CompressionType = {
+    if (getCompressOutput(context)) {
+      val typeValue = context.getConfiguration.get("mapred.output.compression.type",CompressionType.RECORD.toString)
+      CompressionType.valueOf(typeValue)
+    }
+    else CompressionType.NONE;
+  }
+
+  // Get the CompressionCodec for the output SequenceFile
+  def getCompressionCodec(context : TaskAttemptContext) : CompressionCodec = {
+    if (getCompressOutput(context)) {
+      val codecClass = getOutputCompressorClass(context, classOf[DefaultCodec])
+      ReflectionUtils.newInstance(codecClass, context.getConfiguration)
+    }
+    else null
   }
 }
