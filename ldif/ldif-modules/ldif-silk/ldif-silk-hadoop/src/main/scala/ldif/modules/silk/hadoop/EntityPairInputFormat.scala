2c5f58f2c48f9a219301b22dd7c9aca4d2376968
a-matteini <a.matteini@mes-info.de>
2011-12-15 18:03:00 +0100

    Added licence header

--- ldif/ldif-modules/ldif-silk/ldif-silk-hadoop/src/main/scala/ldif/modules/silk/hadoop/EntityPairInputFormat.scala
+++ ldif/ldif-modules/ldif-silk/ldif-silk-hadoop/src/main/scala/ldif/modules/silk/hadoop/EntityPairInputFormat.scala
@@ -1,11 +1,28 @@
+/*
+ * LDIF
+ *
+ * Copyright 2011 Freie Universität Berlin, MediaEvent Services GmbH & Co. KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ldif.modules.silk.hadoop
 
 import org.apache.hadoop.mapreduce._
 import ldif.entity.EntityWritable
 import lib.input.{FileSplit, SequenceFileInputFormat}
 import scala.collection.JavaConversions._
-import collection.mutable.Buffer
-import org.apache.hadoop.io.{Writable, BooleanWritable, IntWritable, NullWritable}
+import org.apache.hadoop.io.{Writable, BooleanWritable}
 import java.io.{DataInput, DataOutput}
 
 class EntityPairInputFormat extends InputFormat[BooleanWritable, EntityPairWritable] {
