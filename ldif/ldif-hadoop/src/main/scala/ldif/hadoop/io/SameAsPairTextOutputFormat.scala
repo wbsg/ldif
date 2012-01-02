378e91668b889aee186e8b50c49fb86a2d2cc72c
a-matteini <a.matteini@mes-info.de>
2012-01-02 16:49:44 +0100

    Updated license header

--- ldif/ldif-hadoop/src/main/scala/ldif/hadoop/io/SameAsPairTextOutputFormat.scala
+++ ldif/ldif-hadoop/src/main/scala/ldif/hadoop/io/SameAsPairTextOutputFormat.scala
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2011 Freie Universität Berlin, MediaEvent Services GmbH & Co. KG 
+ * Copyright 2011-2012 Freie Universität Berlin, MediaEvent Services GmbH & Co. KG
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
