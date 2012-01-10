cf909b868bae6020b535661b4c6b0b821e5f53a5
Andreas Schultz <a.schultz@fu-berlin.de>
2012-01-10 17:11:30 +0100

    Modified LDIF hadoop run scripts to work with dispatcher

--- ldif/bin/ldif-hadoop.bat
+++ ldif/bin/ldif-hadoop.bat
@@ -1,1 +1,1 @@
-java -server -Xmx4G -Xms256M -Dlog4j.configuration=file:resources\log4j.properties -jar ldif-hadoop-executor\target\ldif-hadoop-exe-0.4-jar-with-dependencies.jar $*
+java -server -Xmx4G -Xms256M -Dlog4j.configuration=file:resources\log4j.properties -jar ldif-hadoop-executor\target\ldif-hadoop-exe-0.4-jar-with-dependencies.jar scheduler $*
