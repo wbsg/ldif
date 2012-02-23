a97638dc89cc2d77fa6ac041219e83924e759bea
hannesmuehleisen <hannes@muehleisen.org>
2012-02-23 15:44:40 +0100

    xml parsing for fusion part now working

---
+++ ldif/ldif-modules/ldif-sieve/ldif-sieve-common/src/main/test/scala/ldif/modules/sieve/fusion/FusionConfigTest.scala
@@ -0,0 +1,49 @@
+package ldif.modules.sieve.fusion
+
+import functions.{KeepFirst, PassItOn}
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+import ldif.entity.Restriction.Condition
+import ldif.entity.{Node, Path, EntityDescription}
+import ldif.util.Prefixes
+
+/**
+ * Tests the creation of a fusion configuration from an xml configuration file
+ *
+ * @author Hannes Muehleisen
+ */
+
+class FusionConfigTest extends FlatSpec with ShouldMatchers {
+
+  val fusionXml = <Fusion name="bla" description="blubb">
+    <Class name="dbpedia:Settlement">
+      <Property name="rdfs:label">
+          <FusionFunction class="PassItOn"/>
+      </Property>
+      <Property name="dbpedia-owl:areaTotal">
+          <FusionFunction class="KeepFirst"
+                          metric="sieve:reputation"/>
+      </Property>
+
+    </Class>
+  </Fusion>
+
+  val prefixes = new Prefixes(Map(("rdfs", "http://example.com/rdfs"), ("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"), ("dbpedia", "http://example.com/dbpedia"), ("dbpedia-owl", "http://example.com/dbpedia-owl")))
+
+  val classNode = Node.createUriNode(prefixes.resolve("dbpedia:Settlement"))
+  val condition = new Condition(Path.parse("?a/rdf:type")(prefixes), Set(classNode))
+
+  val entityDescription = new EntityDescription(new ldif.entity.Restriction(Option(condition)), IndexedSeq(IndexedSeq(Path.parse("?a/rdfs:label")(prefixes)), IndexedSeq(Path.parse("?a/dbpedia-owl:areaTotal")(prefixes))))
+
+  val mcFusion = new FusionConfig(IndexedSeq(new FusionSpecification(IndexedSeq(new PassItOn, new KeepFirst), IndexedSeq(prefixes.resolve("rdfs:label"), prefixes.resolve("dbpedia-owl:areaTotal")))), IndexedSeq(entityDescription))
+  val gFusion = FusionConfig.fromXML(fusionXml)(prefixes)
+
+  it should "create the correct fusion configuratioon from XML" in {
+    (gFusion.entityDescriptions) should equal(mcFusion.entityDescriptions)
+    (gFusion.fusionSpecs) should equal(mcFusion.fusionSpecs)
+    (gFusion) should equal(mcFusion)
+  }
+
+}
+
+
