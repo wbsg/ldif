4654ff8e6c77e3272de205362197cff05832b922
a-matteini <a.matteini@mes-info.de>
2012-11-08 12:55:14 +0100

    Removed unused wikidata code

--- ldif/wikidata-wizard/src/main/scala/ldif/wikidata/util/WikidataConsts.scala
+++
@@ -1,7 +0,0 @@
-package ldif.wikidata.util
-
-object WikidataConsts {
-  val wikidataQ = "http://wikidata/qualifier/"
-  val wikidataS = "http://wikidata/statement/"
-  val wikidataV = "http://wikidata/value/"
-}
