0bde4c6cf124ee5fec2c3beeea68a52bc3f5b946
Andreas Schultz <a.schultz@fu-berlin.de>
2012-11-14 12:22:48 +0100

    Adapted DOS batch files to new version

--- ldif/bin/ldif.bat
+++ ldif/bin/ldif.bat
@@ -1,1 +1,1 @@
-java -server -Xmx2G -Xms256M -Dlog4j.configuration=file:resources\log4j.properties -jar ldif-singlemachine\target\ldif-single-0.5-jar-with-dependencies.jar %*
+java -server -Xmx2G -Xms256M -Dlog4j.configuration=file:resources\log4j.properties -cp ldif-singlemachine\target\ldif-single-0.5.1-jar-with-dependencies.jar ldif.local.Ldif %*
