63edb2089a6d24ae0eb521e4122908c41fa5ba20
amatteini <a.matteini@mes-semantics.com>
2014-02-17 14:05:54 +0100

    Fixed bat script

--- ldif/bin/ldif-hadoop-integrate.bat
+++ ldif/bin/ldif-hadoop-integrate.bat
@@ -1,1 +1,1 @@
-java -server -Xmx4G -Xms256M -Dlog4j.configuration=file:resources\log4j.properties -jar ldif-hadoop-executor\target\ldif-hadoop-exe-*-jar-with-dependencies.jar integrate $*
+java -server -Xmx4G -Xms256M -Dlog4j.configuration=file:resources\log4j.properties -jar ldif-hadoop-executor\target\ldif-hadoop-exe-0.5.2-jar-with-dependencies.jar integrate $*
--- ldif/bin/ldif.bat
+++ ldif/bin/ldif.bat
@@ -1,1 +1,1 @@
-java -server -Xmx2G -Xms256M -Dlog4j.configuration=file:resources\log4j.properties -cp "ldif-singlemachine\target\ldif-single-*-jar-with-dependencies.jar" ldif.local.Ldif %*
+java -server -Xmx2G -Xms256M -Dlog4j.configuration=file:resources\log4j.properties -cp ldif-singlemachine\target\ldif-single-0.5.2-jar-with-dependencies.jar ldif.local.Ldif %*
